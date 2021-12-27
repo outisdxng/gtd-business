3e5dce39f26287e9c701f2605024e119dccbbee9
outisxjs <dinhlongit24@gmail.com>
2021-12-27 17:35:45 +0700

    Done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
+function initLazyload() {
+  return new LazyLoad({
+    elements_selector: ".lazy",
+  });
+}
+
 function languageToggle() {
   $(".header__language .header__language-current").on("click", function (e) {
     $(this).parents(".header__language").toggleClass("active");
@@ -73,17 +79,61 @@ function equalHeightElement(el) {
     $(el).height("100px");
   }
 }
+
+function caseStudyScrollToSection() {
+  $(".gtd-case-study-1 .navigation__item").on("click", function () {
+    const idx = $(this).index();
+    const offset = $(".gtd-case-study-1 .content__item").eq(idx).length
+      ? $(".gtd-case-study-1 .content__item").eq(idx).offset().top
+      : null;
+    const heightDismiss = $(window).height() / 5;
+    const menuHeight = $(".header").height();
+    if (offset) {
+      $("body, html").animate(
+        {
+          scrollTop: offset - heightDismiss - menuHeight,
+        },
+        1000
+      );
+    }
+  });
+}
+
+function caseStudyScrollListener() {
+  if ($(".gtd-case-study-1 .navigation").length) {
+    $(".gtd-case-study-1 .navigation__item").each(function (idx) {
+      const offset = $(".gtd-case-study-1 .content__item").eq(idx).length
+        ? $(".gtd-case-study-1 .content__item").eq(idx).offset().top
+        : null;
+      const heightDismiss = $(window).height() / 5;
+      const menuHeight = $(".header").height();
+
+      if (
+        offset &&
+        $(window).scrollTop() >= offset - heightDismiss - menuHeight
+      ) {
+        $(".gtd-case-study-1 .navigation__item").removeClass("active");
+        $(this).addClass("active");
+      }
+    });
+  }
+}
+
 $(document).ready(function () {
+  initLazyload();
   mobileMenuToggle();
   scrollToActiveHeader();
   languageToggle();
   mobileMappingListener();
   mobileDropdown();
   homeAccordion();
+  caseStudyScrollToSection();
+  caseStudyScrollListener();
   equalHeightElement(".gtd-btm-2 .content__item h4");
   equalHeightElement(".gtd-btm-3 .solution__text");
 });
 
 $(window).on("scroll", function () {
   scrollToActiveHeader();
+  caseStudyScrollListener();
 });
